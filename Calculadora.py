3ab9624003264dc6a544c0552a0d9173cdc1705c
Pagno <jesusjuanrex13@gmail.com>
2023-04-21 17:46:39 -0600

    Quitando un pequeño bug y ultimo

--- Calculadora.py
+++ Calculadora.py
@@ -3,6 +3,5 @@ answer, cantidad = 1, 0
 for cantidad in range(products):
     cant = int(input(f"Costo Producto {cantidad}: "))
     answer += cant
-answer = answer-1
-print(answer) 
+answer = answer-1 
 print("Total mas IVA: ", ((answer-((answer/100)*15))+(answer*.16))) if answer >= 2000 else print("Total mas IVA: ", answer*.16+answer)
