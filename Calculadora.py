780732ecefa72e2a82843f7d66abb076b20700e2
Pagno <jesusjuanrex13@gmail.com>
2023-04-21 17:11:19 -0600

    Corrigiendo bugs del sistema de descuento

--- Calculadora.py
+++ Calculadora.py
@@ -4,4 +4,4 @@ for cantidad in range(products):
     cant = int(input(f"Costo Producto {cantidad}: "))
     answer += cant
 answer = answer-1
-print("Total mas IVA: ", (((answer*.16+answer)/100)*10)+answer) if answer >= 2000 else print("Total mas IVA: ", answer*.16+answer)
+print("Total mas IVA: ", (((answer/100)*15)-answer*.16) if answer >= 2000 else print("Total mas IVA: ", answer*.16+answer)
