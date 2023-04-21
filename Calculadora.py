a8fa533406cfa8cb99a5330b1bde998c9aa010b0
Juan Juarez <76190243+Bobionwa@users.noreply.github.com>
2023-04-21 17:03:00 -0600

    Mejorando Codigo De Sistema De Descuento
    I'm the shit

--- Calculadora.py
+++ Calculadora.py
@@ -1,7 +1,7 @@
 products = int(input("Ingrese la cantidad de Productos: "))
-answer, cantidad = 1, = 0
+answer, cantidad = 1, 0
 for cantidad in range(products):
     cant = int(input(f"Costo Producto {cantidad}: "))
     answer += cant
-
-print("Total mas IVA: ", (((answer* .16 + answer)/100)*10)) if answer >= 2000 else print("Total mas IVA: ", answer* .16 + answer)
+answer = answer-1
+print("Total mas IVA: ", (((answer*.16+answer)/100)*10)+answer) if answer >= 2000 else print("Total mas IVA: ", answer*.16+answer)
